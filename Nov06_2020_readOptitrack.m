fcf15bf2784dc39d10f5d85ef05b34ca36055f12
Jo Carpenter <jordannc@stud.ntnu.no>
2021-01-11 11:43:00 -0500

    optitrack dataset

--- Nov06_2020_readOptitrack.m
+++ Nov06_2020_readOptitrack.m
@@ -72,5 +72,121 @@ for sessNum = 1:totalSessions
     end
 end
 
+%% make a struct for each session
+clear optiData
+for i = 1:length(optiTrack)
+    if ~isempty(optiTrack{1,i})
+        % grab the table for this session
+        clear self
+        t = optiTrack{1,i};
+        
+        % Optitrack Motive use a Y-up right handed coordinate system, which
+        % means Y is up and positive X axis is to the left in the original data.
+        % I prefer to use a Z-up left handed system, which means Z is up and
+        % positive X axis is to the right. So, while we assign the variables we also
+        % translate between these reference frames; subtracting the x axis to get a
+        % positive x to the right, and switching out the Y and Z axis:
+        self.timestamp = table2array(t(:,2));
+        self.q.x = table2array(t(:,3));
+        self.q.y = table2array(t(:,5));
+        self.q.z = table2array(t(:,4));
+        self.q.w = table2array(t(:,6));
+        self.position.x = table2array(t(:,7));
+        self.position.y = table2array(t(:,9));
+        self.position.z = table2array(t(:,8));
+        self.fs = round(1/mode(diff(self.timestamp))); % in hz
+        self.n_frame = numel(self.timestamp);
+        
+        self.q.x = -self.q.x;
+        self.position.x = -self.position.x;
+        
+        % calculate movement speed, direction, and angle
+        self.movement = calculate_movement(self);
+        
+        % calculate rotation matrix
+        self.rotation_matrix = calculate_rotation_matrix(self.q);
+        
+        % calculate yaw (azimuth), pitch and roll allocentric head direcitons:
+        self.direction = calculate_direction(self);
+        
+        % simplify calculations by referencing (put this into the self.movement struct?)
+        dv_x = squeeze(self.rotation_matrix(:, 1, :))';
+        dv_y = squeeze(self.rotation_matrix(:, 2, :))';
+        dv_z = squeeze(self.rotation_matrix(:, 3, :))';
+        
+        x = self.position.x;
+        y = self.position.y;
+        z = self.position.z;
+
+        % Add position values to unit vectors for animating the rigid body axis at
+        % the location of the animal in 3D.
+        unit_vec_length = 6;  % length of the unit vector in animation.
+
+        self.x_vector.x = [x, x + dv_x(:, 1) * unit_vec_length];
+        self.x_vector.y = [y, y + dv_x(:, 2) * unit_vec_length];
+        self.x_vector.z = [z, z + dv_x(:, 3) * unit_vec_length];
+
+        self.y_vector.x = [x, x + dv_y(:, 1) * unit_vec_length];
+        self.y_vector.y = [y, y + dv_y(:, 2) * unit_vec_length];
+        self.y_vector.z = [z, z + dv_y(:, 3) * unit_vec_length];
+
+        self.z_vector.x = [x, x + dv_z(:, 1) * unit_vec_length];
+        self.z_vector.y = [y, y + dv_z(:, 2) * unit_vec_length];
+        self.z_vector.z = [z, z + dv_z(:, 3) * unit_vec_length];
+
+        movement_vector = ...
+            [cos(self.movement.direction), sin(self.movement.direction)] * 10;
+
+        self.m_vector.x = [x, x + movement_vector(:, 1)];
+        self.m_vector.y = [y, y + movement_vector(:, 2)];
+        self.m_vector.z = [z, z];
+
+        % save the struct
+        optiData{1,i} = self;
+        
+    else
+        optiData{1,i} = [];
+    end
+end
+
+
+
+%% match up the spiketimes
+clear ST
+for i = 1:length(SpikeTimes)
+    if ~isempty(optiTrack{1,i})
+        tNow = optiData{1,i}.timestamp;
+    else
+        tNow = pos{1,i}(:,1);
+    end
+    
+        for j = 1:length(SpikeTimes{1,i})
+            stNow = SpikeTimes{1,i}{1,j};
+            stAdj = tNow(knnsearch(tNow, stNow));
+            ST{1,i}{1,j} = stAdj;
+        end
+end
+
+
+
+%% save stuff
+save('D:\Data\Dataset\25398\25398OT.mat', 'optiData', 'ST', '-v7.3')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 

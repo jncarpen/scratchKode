f0f35849fdb613d0cdaa3803b0008994e6cd7e8b
Jo Carpenter <jordannc@stud.ntnu.no>
2021-02-18 12:22:09 -0500

    load AP files
    load position files (only) from adrien peyrache's dataset (downloaded on local drive) so that i can use the trajectory data for simulations

---
+++ scratch20210215_loadPeyrachePosfiles.m
@@ -0,0 +1,18 @@
+%% February 15, 2021
+% Description: Load Adrien Peyrache's TH-1 dataset (position files only)
+
+% make a directory of all position files
+folderpath = 'D:\Data\External Data\Peyrache-TH1\PositionFiles';
+posfile_dir = dir(fullfile(folderpath));
+
+% load and save each position file
+count = 1;
+for fl = length(posfile_dir)
+    if contains(posfile_dir(fl).name, 'Mouse')
+        pathnow = [posfile_dir(fl).folder, '\', posfile_dir(fl).name];
+        position_now = load(pathnow);
+        position_now(position_now == -1) = nan; % set -1 to nan
+        AP.session(count).P = position_now;
+        count = count + 1;
+    end
+end

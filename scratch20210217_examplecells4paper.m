bac1153c1724f2184c226a0bf7b4c8335a3715e2
Jo Carpenter <jordannc@stud.ntnu.no>
2021-03-01 09:15:27 -0500

    example cells
    simulated examples for paper

---
+++ scratch20210217_examplecells4paper.m
@@ -0,0 +1,43 @@
+% load('D:\Data\External Data\Simulation-EX\ego0.mat')
+% load('D:\Data\External Data\Simulation-EX\ego90.mat')
+% load('D:\Data\External Data\Simulation-EX\ego180.mat')
+% load('D:\Data\External Data\Simulation-EX\ego270.mat')
+% load('D:\Data\External Data\Simulation-EX\hd0.mat')
+% load('D:\Data\External Data\Simulation-EX\hd90.mat')
+% load('D:\Data\External Data\Simulation-EX\hd180.mat')
+% load('D:\Data\External Data\Simulation-EX\hd270.mat')
+% load('D:\Data\External Data\Simulation-EX\place.mat')
+% load('D:\Data\External Data\Simulation-EX\placeego90.mat')
+% load('D:\Data\External Data\Simulation-EX\placehd270.mat')
+% load('D:\Data\External Data\Simulation-EX\egodist270.mat')
+
+RP = [75 75];
+pathPlot_hd(P, ST, HD);
+outRM = plot_egoRM(P, ST, RP);
+out = modelMe(P,ST,HD);
+plotMe(out);
+
+plot_MVLmap(P,ST,HD)
+
+figure
+map = analyses.map(P, ST, 'smooth', 2, 'binWidth', 150/75); % calculate tuning curve
+peakRate = nanmax(nanmax(map.z));
+rate_map_title = strcat('peak fr: ', sprintf('%.2f',peakRate));
+plot.colorMap(map.z); shading interp
+pbaspect([1 1 1])
+% colormap(gca,'jet')
+set(gca, 'visible', 'off', 'box', 'off');
+% c2 = colorbar; c2.FontSize = 25;
+set(gca,'xtick',[])
+set(gca,'ytick',[])
+title(rate_map_title, 'FontName', 'Calibri light', 'FontSize', 30, 'FontWeight', 'normal');
+box off
+
+[strain] = binSpikes(P(:,1), ST);
+hdTuning(HD, P, strain);
+tc = analyses.turningCurve(spkhd, poshd, sampleTime, <options>)
+
+
+
+
+
